365686df2eada70f9173f48c62f55143814f50ef
talexcrowell <talexcrowell@gmail.com>
2019-02-04 12:19:41 -0800

    added more polish

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -9,16 +9,20 @@ export default class Portfolio extends React.Component{
           <div className='project-description'>
             <a className='live-link' href='https://the-friend-zone-app.herokuapp.com' target='_blank' rel='noopener noreferrer'><h3>The Friend Zone</h3></a>
             <a className='github-repo' href='https://github.com/the-friendzone-app' target='_blank' rel='noopener noreferrer'><i className='github'/></a>
-            <p>Noted is an application focused on helping users learn musical notation and improve their skills as musician.</p>
-            <p>The learning experience comes from utilizing a spaced repetition algorithm and allowing users to be exposed more frequently to questions they may have missed.</p>
+            <p>The Friend Zone is an approach to social media to help people build platonic friendships through the comfort of basic anonymity.</p>
+            <p>It was created with a team of developers where I took the role of Project Manager. Each member developed a separate feature that was reviewed and refactored with the developer team's decisions and criticisms throughout the project.</p>
+            <p>The Friend Zone is an ongoing project with plans for adding new features to improve user experience and continue making the Friend Zone a safe, comfortable online community.</p>
           </div>
-          <img className='project-img' src='https://i.imgur.com/0rtvoY8.png' alt='Noted' ></img>
+          <img className='project-img' src='https://i.imgur.com/2pX7VOI.png?1' alt='The Friend Zone' ></img>
           <div className='tech-stack'>
             <h4 className='tech-title'>Tech Stack</h4>
             <h4>Front-End:</h4>
             <p className='tech'>React</p>
             <p className='tech'>Redux</p>
             <p className='tech'>CSS</p>
+            <p className='tech'>socket.io</p>
+            <p className='tech'>Enzyme</p>
+            <p className='tech'>Jest</p>
             <h4>Back-End:</h4>
             <p className='tech'>Node</p>
             <p className='tech'>Express</p>
@@ -27,12 +31,14 @@ export default class Portfolio extends React.Component{
             <p className='tech'>Passport</p>
             <p className='tech'>Express</p>
             <p className='tech'>JWT</p>
+            <p className='tech'>Mocha</p>
+            <p className='tech'>Chai</p>
           </div>
         </div>   
         <div className='project'>
           <div className='project-description'>
-            <a className='live-link' href='https://noted-client.herokuapp.com'><h3>Noted</h3></a>
-            <a className='live-link' href='https://noted-client.herokuapp.com'>Github</a>
+            <a className='live-link' href='https://noted-client.herokuapp.com' target='_blank' rel='noopener noreferrer'><h3>Noted</h3></a>
+            <a className='github-repo' href='https://github.com/thinkful-ei25/noted-client-sean-alex' target='_blank' rel='noopener noreferrer'><i className='github'/></a>
             <p>Noted is an application focused on helping users learn musical notation and improve their skills as musician.</p>
             <p>The learning experience comes from utilizing a spaced repetition algorithm and allowing users to be exposed more frequently to questions they may have missed.</p>
           </div>
@@ -56,6 +62,7 @@ export default class Portfolio extends React.Component{
           <div className='project'>
             <div className='project-description'>
               <a className='live-link' href='https://overwatch-buddy.herokuapp.com/'><h3>Overwatch Buddy</h3></a>
+              <a className='github-repo' href='https://github.com/talexcrowell/ow-team-app-client' target='_blank' rel='noopener noreferrer'><i className='github'/></a>
               <p>Overwatch Buddy is an application that allows users to customize, build, and save teams using Heroes from Blizzard's Overwatch.</p> 
               <p>It is a tool to help users get further insight into the mechanics of Overwatch team builds by providing firm number statistics for each team build and allows users to add notes to give personal opinions and plan for future builds. Planned features</p>
             </div>
@@ -74,6 +81,8 @@ export default class Portfolio extends React.Component{
               <p className='tech'>Passport</p>
               <p className='tech'>Express</p>
               <p className='tech'>JWT</p>
+              <p className='tech'>Mocha</p>
+              <p className='tech'>Chai</p>
             </div>
           </div>
       </section>
