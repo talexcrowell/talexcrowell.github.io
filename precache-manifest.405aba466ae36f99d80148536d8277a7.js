3a86d4f823c7aecbda74c285a3cac202a2f3d023
talexcrowell <talexcrowell@gmail.com>
2019-02-07 10:21:48 -0800

    Updates

---
+++ precache-manifest.405aba466ae36f99d80148536d8277a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1738f9114a8fce218759",
+    "url": "/static/js/main.1738f911.chunk.js"
+  },
+  {
+    "revision": "7aa5281fac2ebec48ec4",
+    "url": "/static/js/1.7aa5281f.chunk.js"
+  },
+  {
+    "revision": "1738f9114a8fce218759",
+    "url": "/static/css/main.569d24cf.chunk.css"
+  },
+  {
+    "revision": "8b4478d5baf86c6d9da17883a7a58629",
+    "url": "/index.html"
+  }
+];
