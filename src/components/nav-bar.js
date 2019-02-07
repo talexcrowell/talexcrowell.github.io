85f901559d0a13a54e502cfd5719a4c5915c2178
talexcrowell <talexcrowell@gmail.com>
2019-02-07 10:14:48 -0800

    github pages

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -9,7 +9,7 @@ export default class NavBar extends React.Component{
           <li className='nav-direction'><a href='https://docs.google.com/document/d/1h5_sHImeKSV5LLLN5Pat8KDdU8bqAvYXHSJL7hCCiuU/edit?usp=sharing' target='_blank' rel="noopener noreferrer" download><i className='nav-resume'/></a></li>
           <li className='nav-direction'><a href='mailto:talexcrowell@gmail.com'><i className='nav-email'/></a></li>
           <li className='nav-direction-right'><a href='#contact'>Contact</a></li>
-          <li className='nav-direction-right'><a href='#about'>About Me</a></li>
+          <li className='nav-direction-right'><a href='#about'>About Me</a></li> 
           <li className='nav-direction-right'><a href='#portfolio'>Portfolio</a></li>
         </ul> 
       </section>
